9db25a02393ca043b7d269344b329cf88498be7c
MartinK <82144965+MartinKruess@users.noreply.github.com>
2024-04-10 23:20:26 +0200

    update streamdates (#1)
    Co-authored-by: MartinK <82144965+RaikunGZ@users.noreply.github.com>

--- src/global/termine.jsx
+++ src/global/termine.jsx
@@ -1,33 +1,42 @@
 export const Termine = () => {
-    const dateArr = [
-        {
-            title: "Halloween Night Stream",
-            details: "Wir feiern Helloween im Stream.",
-            date: "31.10.2023",
-        },
-        {
-            title: "Rocket League Turnier",
-            details: "Die StreamingIndustry veranstaltet ein Rocket League turnier und ich bin dabei.",
-            date: "04-05.11.2023",
-        },
-        {
-            title: "Sylvester",
-            details: "Wir feiern Neujahr gemeinsam im Stream.",
-            date: "31.12.2023",
-        }
-    ]
+  const dateArr = [
+    {
+      title: "Rocket League Turnier",
+      details:
+        "Ich Plane ein Rocket League Turnier zu veranstalten mit einigen Streamern und Zuschauern.",
+      date: "06.2024",
+    },
+    {
+      title: "Gamescom 2024",
+      details:
+        "Falls möglich möchte ich dieses jahr wieder auf die Gamescom. Gerne auch in eienr größeren Gruppe, falls jemand mitkommen möchte.",
+      date: "23-24.08.2024",
+    },
+    {
+      title: "12-24h Stream (in Planung)",
+      details:
+        "Wie jedes Jahr bin ich am überlegen ob ich (am Feiertag 03.10) einen 12-24h Stream machen soll.",
+      date: "03.10.2024",
+    },
+    {
+      title: "Halloween Horror Stream (in Planung)",
+      details:
+        "Wie jedes Jahr bin ich am überlegen ob ich (am Feiertag 31.10) einen 12-24h Stream machen soll.",
+      date: "31.10.2024",
+    },
+  ];
 
-    return(
-        <article className="terminContainer">
-            {dateArr.map((date) => (
-                <div className="dateBG">
-                    <div className="date">
-                        <h2>{date.title}</h2>
-                        <p>{date.details}</p>
-                        <p className="dateText">{date.date}</p>
-                    </div>
-                </div>
-            ))}
-        </article>
-    )
-}
+  return (
+    <article className="terminContainer">
+      {dateArr.map((date) => (
+        <div className="dateBG">
+          <div className="date">
+            <h2>{date.title}</h2>
+            <p>{date.details}</p>
+            <p className="dateText">{date.date}</p>
+          </div>
+        </div>
+      ))}
+    </article>
+  );
+};
